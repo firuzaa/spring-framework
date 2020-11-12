ec486b6b09c926e96e4e1906b80bac2b5d30bdad
feruzaabduwayit <feruza95@hotmail.com>
2020-11-11 23:44:22 -0500

    Spring Boot - Carpet Exercise

---
+++ Spring-07-SpringBoot-CarpetExercise/src/main/java/com/company/services/carpet/CarpetVA.java
@@ -0,0 +1,13 @@
+package com.company.services.carpet;
+
+import com.company.enums.City;
+import com.company.interfaces.carpetPrices.Carpet;
+
+import java.math.BigDecimal;
+
+public class CarpetVA implements Carpet {
+    @Override
+    public BigDecimal getSqFtPrice(City city) {
+        return null;
+    }
+}
