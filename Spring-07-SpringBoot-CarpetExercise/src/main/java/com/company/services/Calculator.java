85afe15c29057196697613acddddffdd76833bcc
feruzaabduwayit <feruza95@hotmail.com>
2020-11-12 00:20:49 -0500

    Spring Boot - Carpet Exercise

--- Spring-07-SpringBoot-CarpetExercise/src/main/java/com/company/services/Calculator.java
+++ Spring-07-SpringBoot-CarpetExercise/src/main/java/com/company/services/Calculator.java
@@ -1,4 +1,34 @@
 package com.company.services;
 
+import com.company.enums.City;
+import com.company.interfaces.carpetPrices.Carpet;
+import com.company.interfaces.floorTypes.Floor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Component
 public class Calculator {
+
+    @Qualifier("carpetTX")
+    @Autowired
+    private Carpet carpet;
+
+    @Qualifier("bedroom")
+    @Autowired
+    private Floor floor;
+
+    public String getTotalCarpetCost(City city) throws Exception {
+        BigDecimal cost = carpet.getSqFtPrice(city).multiply(floor.getArea());
+
+        if (cost.compareTo(BigDecimal.ZERO)==0){
+            throw new Exception("This city does not exist");
+        }
+
+        return "Total cost for carpet: " + cost;
+
+    }
+
 }
