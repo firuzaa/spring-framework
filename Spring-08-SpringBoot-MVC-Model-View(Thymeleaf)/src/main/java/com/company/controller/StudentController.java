dbe49f3b43bb71746138d67a9dc9450e2b5470f6
feruzaabduwayit <feruza95@hotmail.com>
2020-12-01 21:50:50 -0500

    MVC Model-View

---
+++ Spring-08-SpringBoot-MVC-Model-View(Thymeleaf)/src/main/java/com/company/controller/StudentController.java
@@ -0,0 +1,41 @@
+package com.company.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+@Controller
+@RequestMapping("/student")
+public class StudentController {
+
+    @GetMapping("/welcome")
+    public String homepage(Model model){
+
+        model.addAttribute("name","Feruza");
+        model.addAttribute("age","25");
+        model.addAttribute("course","MVC");
+
+        String subject = "Collections";
+        model.addAttribute("subject",subject);
+
+        int stuID = new Random().nextInt(1000);
+        model.addAttribute("stuID",stuID);
+
+        List<Integer> number = new ArrayList<>();
+        number.add(5);
+        number.add(4);
+        number.add(6);
+        model.addAttribute("number",number);
+
+        LocalDate bday = LocalDate.now().minusYears(25);
+        model.addAttribute("bday",bday);
+
+        return "student/welcome";
+    }
+}
